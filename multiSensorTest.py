14fdfed8b0af232156bc23ba297f3e348a578048
sidoschn <dominik.schneidereit@fau.de>
2026-03-09 13:21:35 +0100

    added testing script for multi sensor detection

---
+++ multiSensorTest.py
@@ -0,0 +1,10 @@
+from ds18b20 import DS18B20
+
+sensors = []
+for sensor_id in DS18B20.get_available_sensors():
+    sensors.append(DS18B20(sensor_id))
+
+print (len(sensors))
+
+for sensor in sensors:
+    print("Sensor %s has temperature %.2f" % (sensor.get_id(), sensor.get_temperature()))
